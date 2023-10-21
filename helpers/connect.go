2c90479cb5098426e8953ff33560a18628654eca
imago-dzarne <drdlemmer@gmail.com>
2023-10-21 16:29:27 +0300

    Add mongodb_uri to kaktus-api service

--- helpers/connect.go
+++ helpers/connect.go
@@ -5,16 +5,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectDB() *mongo.Client {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environment variable.")

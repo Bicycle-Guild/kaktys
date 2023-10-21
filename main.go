442e8913930122d72c3489e044b7fea8782daf81
Eduard Lemmer <148285955+imago-dzarne@users.noreply.github.com>
2023-10-21 17:15:48 +0300

    Cors fix (#4)
    * Add cors HEADERS
    
    * Change Header

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func header_middleware(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		f(w, r)
 	}

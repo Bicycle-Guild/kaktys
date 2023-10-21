69ef0f55976d3b6032a6114fd10e29cc4449b0b6
Eduard Lemmer <148285955+imago-dzarne@users.noreply.github.com>
2023-10-21 17:03:57 +0300

    Add cors HEADERS (#3)

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func json_middleware(f http.HandlerFunc) http.HandlerFunc {
+func header_middleware(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		f(w, r)
 	}
@@ -17,9 +19,9 @@ func json_middleware(f http.HandlerFunc) http.HandlerFunc {
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/subjects", json_middleware(get_subjects_http)).Methods("GET")
-	r.HandleFunc("/api/subjects/{subjectID}", json_middleware(get_subject_http)).Methods("GET")
-	r.HandleFunc("/api/subjects", json_middleware(create_subject_http)).Methods("POST")
+	r.HandleFunc("/api/subjects", header_middleware(get_subjects_http)).Methods("GET")
+	r.HandleFunc("/api/subjects/{subjectID}", header_middleware(get_subject_http)).Methods("GET")
+	r.HandleFunc("/api/subjects", header_middleware(create_subject_http)).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
